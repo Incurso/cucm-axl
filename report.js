5a3317802907960fbe49a9a85113fd1dc13963c6
Einar Sigurðsson <einarsi@landspitali.is>
2021-12-14 09:12:33 +0000

    UPDATE: added help and fixed linting

--- report.js
+++ report.js
@@ -1,20 +1,23 @@
 import fs from 'fs'
 import path from 'path'
-import cliProgress from 'cli-progress'
-import chalk from 'chalk'
 import yaml from 'js-yaml'
 import parseArgs from 'minimist'
 import pg from 'pg'
-import { start } from 'repl'
 import Excel from 'exceljs'
-
-//import AXL from './utils/cucm-axl.js'
-
-//const axl = new AXL()
 
 const args = parseArgs(process.argv.slice(2))
 
-console.log(args)
+// Display help and exit if
+if (args.help) {
+  console.log(`Usage: node ${path.basename(process.argv.slice(1, 2).toString())} [OPTION]\n`)
+  console.log(`${'--config <inputfilename>'.padEnd(35)} Load YAML config file.`)
+  console.log(`${'--startDate'.padEnd(35)} Start date.`)
+  console.log(`${'--endDate'.padEnd(35)} End date.`)
+  console.log(`${'--interval'.padEnd(35)} Hours or days.`)
+  console.log(`${'--dn'.padEnd(35)} Diractory Number.`)
+  console.log('')
+  process.exit(0)
+}
 
 const startDate = args['start-date']
   ? new Date(args['start-date'])
@@ -25,7 +28,6 @@ if (startDate.toString() === 'Invalid Date') {
   throw new Error('Invalid start-date')
 }
 
-//d.setHours(24,0,0,0)
 const endDate = args['end-date']
   ? new Date(args['end-date'])
   : new Date(new Date().setHours(24, 0, 0, 0))
@@ -35,13 +37,11 @@ if (endDate.toString() === 'Invalid Date') {
   throw new Error('Invalid end-date')
 }
 
-const interval = args['interval'] || 'day'
-const dn = args['dn'] || '1800'
+const interval = args.interval || 'day'
+const dn = args.dn || '1800'
 
 // Load config file
 const config = yaml.load(fs.readFileSync(path.resolve(args.config || './config/config.yml'), 'utf8')).CDR_REPORT
-
-//console.log(config)
 
 const client = new pg.Client({
   host: config.HOST,
@@ -63,7 +63,7 @@ const saveToExcel = async (data) => {
     { width: 12 },
     { width: 12 },
     { width: 12 },
-    { width: 12 },
+    { width: 12 }
   ]
 
   worksheet.getCell('A1').value = `Number: ${dn}`
@@ -77,7 +77,7 @@ const saveToExcel = async (data) => {
     totalsRow: true,
     style: {
       theme: 'TableStyleMedium2',
-      showRowStripes: true,
+      showRowStripes: true
     },
     columns: [
       { name: 'DateTime', totalsRowLabel: 'Totals:', filterButton: true },
@@ -85,17 +85,20 @@ const saveToExcel = async (data) => {
       { name: 'Answered', totalsRowFunction: 'sum', filterButton: false },
       { name: 'Forwarded To', totalsRowFunction: 'sum', filterButton: false },
       { name: 'Forwarded From', totalsRowFunction: 'sum', filterButton: false },
-      { name: 'Unanswered', totalsRowFunction: 'sum', filterButton: false },
+      { name: 'Unanswered', totalsRowFunction: 'sum', filterButton: false }
     ],
-    rows: data.rows.map(row => Object.values(row).map(i => !isNaN(i) ? parseInt(i) : i)),
-  });
+    rows: data.rows.map(row => Object.values(row).map(i => !isNaN(i) ? parseInt(i) : i))
+  })
 
   await workbook.xlsx.writeFile(fileName)
 }
 
 await client.connect()
   .then(() => console.log('connected'))
-  .catch((err) => console.error('connection error', err.stack))
+  .catch((err) => {
+    console.error('Connection Error:', err.message)
+    process.exit(1)
+  })
 
 const query = `
   SELECT
@@ -154,11 +157,8 @@ const query = `
   ORDER BY d.date, d.${interval};
 `
 
-console.log(query)
-
 console.time('query')
 await client.query(query)
-  //.then((result) => console.log(result.rows))
   .then(async (result) => {
     await saveToExcel(result)
   })
